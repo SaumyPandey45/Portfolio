0d79f189e675328abfd7dc07de865d8bb5126dcb
Saumy Pandey <saumya00045@gmail.com>
2025-10-05 23:15:22 +0530

    Fix CV download link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="src\assets\Saumy Resume..pdf"
+                href="public\assets\Saumy Resume..pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
